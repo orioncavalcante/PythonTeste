e3aa5e7895581c625f48155c8eb985c77c63d047
orioncavalcante <orion.franca19@gmail.com>
2024-12-26 23:21:24 -0300

    main

--- CURSO_PYTHON/main.py
+++ CURSO_PYTHON/main.py
@@ -1,1 +1,22 @@
- 
+import unittest
+class Calculator:
+    def add(self, a, b):
+        return a + b
+
+    def subtract(self, a, b):
+        return a - b
+
+class TestCalculator(unittest.TestCase):
+    def setUp(self):
+        self.calc = Calculator()
+
+    def test_add(self):
+        self.assertEqual(self.calc.add(2, 3), 5)
+        self.assertEqual(self.calc.add(-1, 1), 0)
+
+    def test_subtract(self):
+        self.assertEqual(self.calc.subtract(5, 3), 2)
+        self.assertEqual(self.calc.subtract(0, 4), -4)
+
+if __name__ == "__main__":
+    unittest.main()
